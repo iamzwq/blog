df26990ae970f9318bd201195d3d3829fc2d55ea
iamzwq <876867644@qq.com>
2023-03-12 01:24:57 +0800

    啊啊啊，气死了

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/blog/",
   title: "张文强 - 个人博客",
   description: "个人博客",
   themeConfig: {
