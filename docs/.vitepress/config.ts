ab23c5855117a796aa6c21c897d9673c20495558
iamzwq <876867644@qq.com>
2023-03-12 10:16:38 +0800

    update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,30 +1,44 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: "/blog/",
-  title: "张文强 - 个人博客",
-  description: "个人博客",
+  title: "相当哇塞的博客",
+  description: "再多一眼看一眼就会爆炸 💥",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
+      { text: "主页", link: "/" },
+      { text: "前端", link: "/frontend/原型链和继承" },
+      { text: "VSCode", link: "/vscode/index" },
       {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' },
-          { text: '小黑子', link: '/xiaoheizi' }
-        ]
-      }
+        text: "我的掘金",
+        link: "https://juejin.cn/user/3087084382068286/posts",
+      },
     ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+    sidebar: {
+      "/frontend": [
+        {
+          text: "前端",
+          items: [
+            { text: "原型链和继承", link: "/frontend/原型链和继承" },
+            { text: "常用工具函数", link: "/frontend/工具函数" },
+          ],
+        },
+      ],
+      "/vscode": [
+        {
+          text: "VSCode",
+          items: [
+            { text: "插件", link: "/vscode/index" },
+            { text: "设置文件", link: "/vscode/settings" },
+            { text: "代码片段", link: "/vscode/snippets" },
+          ],
+        },
+      ],
+    },
+
+    socialLinks: [{ icon: "github", link: "https://github.com/iamzwq" }],
+  },
+});
